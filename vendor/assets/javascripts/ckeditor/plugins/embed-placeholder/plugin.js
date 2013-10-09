3c6e7e60f2669685bb657835b36e34ec3330806d
Bryan Ricker <bricker88@gmail.com>
2013-10-08 19:00:10 -0700

    Clean up the embeditor ckeditor modal

--- vendor/assets/javascripts/ckeditor/plugins/embed-placeholder/plugin.js
+++ vendor/assets/javascripts/ckeditor/plugins/embed-placeholder/plugin.js
@@ -10,11 +10,11 @@
 
 
 (function() {
-    var pluginName      = 'embed-placeholder',
-        dialogName      = 'embed-placeholder-dialog',
-        command         = 'openEmbedPlaceholderDialog',
-        button          = 'EmbedPlaceholder', // icon must match this, lowercased.
-        icon            = 'embedplaceholder';
+    var pluginName = 'embed-placeholder',
+        dialogName = 'embed-placeholder-dialog',
+        command    = 'openEmbedPlaceholderDialog',
+        button     = 'EmbedPlaceholder', // icon must match this, lowercased.
+        icon       = 'embedplaceholder';
 
 
     CKEDITOR.plugins.add(pluginName, {
@@ -32,20 +32,22 @@
 
             contents : [
               {
-                id : 'embed',
-                elements : [
+                id        : 'embed',
+                label     : 'Embed Info',
+                elements  : [
                   {
-                    id : "alpha-warning",
+                    id : "alphaWarning",
                     type : 'html',
-                    html : '<span style="color:#ff0000">' +
-                           'This feature is still a WIP, and you may see ' +
-                           'some rendering errors. Please report any problems.</span>'
-                  },
+                    html : '<span style="color:#ff0000"><strong>' +
+                           'This feature is in beta. Please report any ' +
+                           'problems.</strong></span>'
+                  }, // alphaWarning
                   {
                     id          : 'embedUrl',
                     type        : 'text',
-                    label       : 'oEmbed URL (<a href="' +
-                                  'https://github.com/SCPR/oembed-manual/tree/master/services#readme' +
+                    label       : '<strong>oEmbed URL</strong> (<a href="' +
+                                  'https://github.com/SCPR/oembed-manual' +
+                                  '/tree/master/services#readme' +
                                   '" target="_blank">Help</a>)',
                     autofocus   : 'autofocus',
 
@@ -54,16 +56,17 @@
                         alert("URL can't be empty.")
                         return false
                       }
-                    }
-                  },
+                    } // validate
+                  }, // embedUrl
                   {
-                    id : 'embedService',
-                    type : 'select',
-                    label : 'Service<br /><span style="color:#ff0000">' +
-                            'If a service is not listed here, then it is not ' +
-                            'officially supported. You may select "Other" ' +
-                            'to try it out.</span>',
-                    items : [
+                    id      : 'embedService',
+                    type    : 'select',
+                    label   : '<strong>Service</strong><br />' +
+                            '<span style="color:#ff0000">' +
+                            'If a service is not listed here, then it is ' +
+                            'not officially supported. You may select ' +
+                            '"Other" to try it out.</span>',
+                    items   : [
                       ['Select a Service:', ''],
                       ['YouTube', 'youtube'],
                       ['Vimeo', 'vimeo'],
@@ -83,74 +86,100 @@
                       ['Instagram', 'instagram'],
                       ['SoundCloud', 'soundcloud'],
                       ['Spotify', 'spotify'],
-                      ['Other', 'other']
-                    ]
-                  },
+                      ['Other (Embedly)', 'other']
+                    ], // items
+
+                    validate : function() {
+                      if(!this.getValue()) {
+                        alert("Service can't be empty.")
+                        return false
+                      }
+                    } // validate
+                  }, // embedService
                   {
-                    id          : 'linkTitle',
-                    type        : 'text',
-                    label       : 'Link Title'
-                  },
+                    id      : 'linkTitle',
+                    type    : 'text',
+                    label   : 'Link Title (optional) ' +
+                              '<span style="color:#ff0000">' +
+                              'The URL will be used if no title is ' +
+                              'specified.</span>'
+                  } // linkTitle
+                ] // elements
+              }, // embed
+              {
+                id        : 'embed-advanced',
+                label     : 'Advanced',
+                elements  : [
                   {
-                    id          : 'embedMaxHeight',
-                    type        : 'text',
-                    label       : 'Embed Maximum Height (Optional)<br />' +
+                    id      : 'embedMaxHeight',
+                    type    : 'text',
+                    label   : 'Embed Maximum Height (Optional)<br />' +
                                   '<span style="color:#ff0000">' +
                                   'This parameter is not honored by all ' +
-                                  'embeds, and it is recommended that you ' +
+                                  'embeds. It is recommended that you ' +
                                   'leave it blank unless necessary.</span>',
-                    width       : '45px',
+                    width   : '45px',
                     onLoad: function() {
                       $('input', '#'+this.domId).after(' <strong>px</strong>')
                     }
-                  }
-                ]
-              }
-            ],
+                  } // embedMaxHeight
+                ] // elements
+              }, // embed-advanced
+            ], // contents
 
             onOk: function() {
-              var url         = this.getContentElement('embed', 'embedUrl').getValue(),
-                  title       = this.getContentElement('embed', 'linkTitle').getValue(),
-                  maxheight   = this.getContentElement('embed', 'embedMaxHeight').getValue(),
-                  service     = this.getContentElement('embed', 'embedService').getValue();
+              var url         = this.getContentElement(
+                                  'embed', 'embedUrl'
+                                ).getValue(),
+
+                  title       = this.getContentElement(
+                                  'embed', 'linkTitle'
+                                ).getValue(),
+
+                  maxheight   = this.getContentElement(
+                                  'embed-advanced', 'embedMaxHeight'
+                                ).getValue(),
+
+                  service     = this.getContentElement(
+                                  'embed', 'embedService'
+                                ).getValue();
 
               if(title === "") title = url;
 
               var p           = editor.document.createElement('p'),
-                  n           = "\n",
                   markBegin   = "<!-- EMBED PLACEHOLDER: " + url + " -->",
                   markEnd     = "<!-- END PLACEHOLDER -->";
 
               var tagProps = {
-                'href'            : url,
-                'class'           : "embed-placeholder",
-                'title'           : title,
-                'text'            : title
+                'href'    : url,
+                'class'   : "embed-placeholder",
+                'title'   : title,
+                'text'    : title
               }
 
               if(maxheight !== "") tagProps['data-maxheight'] = maxheight;
               if(service !== "") tagProps['data-service'] = service;
 
-              var tag = $("<a />", tagProps);
+              var tag  = $("<a />", tagProps),
+                  html = [markBegin,tag[0].outerHTML,markEnd].join("")
 
-              var html = [markBegin,tag[0].outerHTML,markEnd].join("");
               p.setHtml(html)
               instance.insertElement(p)
-            }
+            } // onOk
           }; // return
-        });
+        }); // dialog.add
 
 
         editor.addCommand(command,
           new CKEDITOR.dialogCommand(dialogName, {
             allowedContent: 'a[*](*)'
           })
-        );
+        )
 
         editor.ui.addButton(button, {
           label     : 'Embed Placeholder',
           command   : command
-        });
+        })
       } // init
-    }); // add
-})(); // closure
+    }) // plugins.add
+})() // closure
