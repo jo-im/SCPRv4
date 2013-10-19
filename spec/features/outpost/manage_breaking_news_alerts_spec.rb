dd7fe67e3b8072a8aefdea36f04ee12dfae2a2b6
Bryan Ricker <bricker88@gmail.com>
2013-10-18 18:19:22 -0700

    Fix breaking news alert request specs

--- spec/features/outpost/manage_breaking_news_alerts_spec.rb
+++ spec/features/outpost/manage_breaking_news_alerts_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe BreakingNewsAlert do
-  let(:valid_record) { build :breaking_news_alert }
-  let(:invalid_record) { build :breaking_news_alert }
-  let(:updated_record) { build :breaking_news_alert }
+  let(:valid_record) { build :breaking_news_alert, headline: "It's hot outside!" }
+  let(:invalid_record) { build :breaking_news_alert, headline: "" }
+  let(:updated_record) { build :breaking_news_alert, headline: "It's raining!" }
 
   it_behaves_like "managed resource"
   it_behaves_like "save options"
