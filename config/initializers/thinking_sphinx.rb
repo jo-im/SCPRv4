79f8dcea96319cba806fd9344b0758b7d2225a5e
Bryan Ricker <bricker88@gmail.com>
2013-10-20 13:54:45 -0700

    Remove TS version check; add back #to_crc32

--- config/initializers/thinking_sphinx.rb
+++ config/initializers/thinking_sphinx.rb
@@ -1,29 +1,24 @@
 # https://github.com/pat/thinking-sphinx/issues/635
-if defined?(ThinkingSphinx::ActiveRecord::DatabaseAdapters::MySQLAdapter)
-  class ThinkingSphinx::ActiveRecord::DatabaseAdapters::MySQLAdapter
-    def time_zone_query_pre
-      []
-    end
+class ThinkingSphinx::ActiveRecord::DatabaseAdapters::MySQLAdapter
+  def time_zone_query_pre
+    []
   end
 end
 
-if defined?(ThinkingSphinx::MysqlAdapter)
-  class ThinkingSphinx::MysqlAdapter
-    def utc_query_pre
-      ""
-    end
+class ThinkingSphinx::RakeInterface
+  def index(reconfigure = true, verbose = true)
+    configure if reconfigure
+
+    dir = configuration.indices_location
+    FileUtils.mkdir_p(dir) unless File.exists?(dir)
+    ThinkingSphinx.before_index_hooks.each { |hook| hook.call }
+    controller.index :verbose => verbose
   end
 end
 
-if defined?(ThinkingSphinx::RakeInterface)
-  class ThinkingSphinx::RakeInterface
-    def index(reconfigure = true, verbose = true)
-      configure if reconfigure
-
-      dir = configuration.indices_location
-      FileUtils.mkdir_p(dir) unless File.exists?(dir)
-      ThinkingSphinx.before_index_hooks.each { |hook| hook.call }
-      controller.index :verbose => verbose
-    end
+# Thinking Sphinx 3 removed this method, but we're still using it.
+class String
+  def to_crc32
+    Zlib.crc32(self)
   end
 end
