21b9dfcd0a3d640e1c855600f3efae557ad0f260
Bryan Ricker <bricker88@gmail.com>
2013-10-19 22:14:14 -0700

    Patch thinking sphinx rake interface to work around File Exists error

--- config/initializers/thinking_sphinx.rb
+++ config/initializers/thinking_sphinx.rb
@@ -14,3 +14,16 @@ if defined?(ThinkingSphinx::MysqlAdapter)
     end
   end
 end
+
+if defined?(ThinkingSphinx::RakeInterface)
+  class ThinkingSphinx::RakeInterface
+    def index(reconfigure = true, verbose = true)
+      configure if reconfigure
+
+      dir = configuration.indices_location
+      FileUtils.mkdir_p(dir) unless File.exists?(dir)
+      ThinkingSphinx.before_index_hooks.each { |hook| hook.call }
+      controller.index :verbose => verbose
+    end
+  end
+end
