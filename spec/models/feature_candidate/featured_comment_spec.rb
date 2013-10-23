4f4a86e94d59e26f68d9332e3aa1b6ba3431e0b8
Bryan Ricker <bricker88@gmail.com>
2013-10-22 21:36:21 -0700

    Add spec for featured comment candidate

--- spec/models/feature_candidate/featured_comment_spec.rb
+++ spec/models/feature_candidate/featured_comment_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe FeatureCandidate::FeaturedComment do
+  let(:bucket) { create :featured_comment_bucket }
+  let(:category) { create :category_news, comment_bucket: bucket }
+
+  describe '#content' do
+    it "is the first featured comment for the category's bucket" do
+      comment = create :featured_comment, :published, bucket: bucket
+
+      candidate = FeatureCandidate::FeaturedComment.new(category)
+      candidate.content.should eq comment
+    end
+  end
 end
