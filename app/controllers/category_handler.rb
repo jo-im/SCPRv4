26bc81dceb573506dfd627efb19d66e39687bfe0
Jeremy Hoffing <jhoffing@scpr.org>
2013-11-06 18:06:06 -0800

    Get featured content from category article association

--- app/controllers/category_handler.rb
+++ app/controllers/category_handler.rb
@@ -4,11 +4,11 @@ module CategoryHandler
   def handle_category
     page      = params[:page].to_i
     per_page  = PER_PAGE
-
     @content = @category.content(
       :page       => page,
       :per_page   => per_page
     )
+    @featured_article = @category.category_articles.first.article
     @twitter_feeds = @category.bios.map(&:twitter_handle)
     respond_with @content, template: "category/show", layout: "vertical"
   end
