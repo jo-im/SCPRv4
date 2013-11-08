ac24555ca98ba3ca59dd7069a4dead9704269c38
Jeremy Hoffing <jhoffing@scpr.org>
2013-11-07 22:04:13 -0800

    Render featured articles, latest event, latest headlines and resources

--- app/controllers/category_handler.rb
+++ app/controllers/category_handler.rb
@@ -8,8 +8,20 @@ module CategoryHandler
       :page       => page,
       :per_page   => per_page
     )
-    @featured_article = @category.articles.first
+    @featured_articles = @category.articles
+    @featured_article = @featured_articles.first
+    @resources = @featured_articles[1..4]
     @featured_image = @featured_article.asset
+
+    @category_articles = @content.map { |a| a.to_article }
+    @latest_articles = @category_articles[1..2]
+
+    if @featured_article.original_object.issues
+      @primary_issue = @featured_article.original_object.issues.first
+    end
+
+    @top_two_issue_articles ||= @primary_issue.articles.first(2)
+    @latest_event = @category.events.first.to_article
     @twitter_feeds = @category.bios.map(&:twitter_handle)
     respond_with @content, template: "category/show", layout: "vertical"
   end
