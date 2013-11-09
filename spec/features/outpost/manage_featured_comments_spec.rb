026d0bac969716c28b356b30de69e7230796ecf8
Bryan Ricker <bricker88@gmail.com>
2013-11-08 19:53:40 -0800

    Remove default status from featured_comments table

--- spec/features/outpost/manage_featured_comments_spec.rb
+++ spec/features/outpost/manage_featured_comments_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe FeaturedComment do
-  let(:valid_record) { build :featured_comment, :published }
-  let(:updated_record) { build :featured_comment, :published }
-  let(:invalid_record) { build :featured_comment, :published, username: "" }
+  let(:valid_record) { build :featured_comment }
+  let(:updated_record) { build :featured_comment }
+  let(:invalid_record) { build :featured_comment, username: "" }
 
   it_behaves_like "managed resource"
   it_behaves_like "save options"
