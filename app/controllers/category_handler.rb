77fdd4b37c3d40cb57be61214bbcdc3514e2fe9b
Jeremy Hoffing <jhoffing@scpr.org>
2013-11-08 20:10:29 -0800

    Check if content exists first

--- app/controllers/category_handler.rb
+++ app/controllers/category_handler.rb
@@ -8,33 +8,37 @@ module CategoryHandler
       :page       => page,
       :per_page   => per_page
     )
-    if @featured_articles = @category.featured_articles
+    if @featured_articles = @category.featured_articles.any?
       @lead_article = @featured_articles.first
       @featured_image ||= @lead_article.asset
       @resources = @featured_articles[1..4]
       @featured_interactive = @featured_articles[5]
+
+      if @issues = @lead_article.original_object.issues.any?
+         @primary_issue = @issues.first
+         @top_two_issue_articles = @primary_issue.articles.first(2)
+      end
+
     end
 
     @category_articles = @content.map { |a| a.to_article }
     @latest_articles = @category_articles[1..2]
 
 
-    if @issues = @lead_article.original_object.issues
-      @primary_issue = @issues.first
-      @top_two_issue_articles = @primary_issue.articles.first(2)
+    if @category_issues = @category.issues.any?
+      @special_issue = @category_issues.first
+      @other_issues = @category_issues[1..2]
+      @top_two_special_issue_articles ||= @special_issue.articles.first(2)
     end
 
-    @category_issues = @category.issues
-    @special_issue = @category_issues.first
-    @other_issues = @category_issues[1..2]
+    if @events = @category.events.published.upcoming.any?
+      @latest_event = @events.first.to_article
+      @upcoming_events = @events[1..3].map { |a| a.to_article }
+    end
 
-    @top_two_special_issue_articles ||= @special_issue.articles.first(2)
-    @events = @category.events.published.upcoming
-    @latest_event = @events.first.to_article
-    @upcoming_events = @events[1..3].map { |a| a.to_article }
-
-    @quote = @category.quotes.published.first
-    @quote_article = @quote.article.to_article
+    if @quote = @category.quotes.published.any?
+      @quote_article = @quote.first.article.to_article
+    end
 
     @twitter_feeds = @category.bios.map(&:twitter_handle)
     respond_with @content, template: "category/show", layout: "vertical"
