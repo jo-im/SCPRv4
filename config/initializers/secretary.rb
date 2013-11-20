12413891185a0872d5f3ce6e13083f1efe333ea0
Bryan Ricker <bricker88@gmail.com>
2013-11-19 16:16:33 -0800

    Fix loading for secretary

--- config/initializers/secretary.rb
+++ config/initializers/secretary.rb
@@ -1,11 +1,13 @@
-# Configuration for Secretary
-require_dependency "secretary"
-
+# First configure, because Secretary::Version uses the confiration
 Secretary.configure do |config|
   config.user_class = "::AdminUser"
 end
 
-# Setup Secretary to be Outposty
+# Then require secretary/version so we know it's loaded before we
+# force-feed it a spoonful of Outpost.
+require 'secretary/version'
+
+# nom nom nom
 Secretary::Version.instance_eval do
   outpost_model
 end
