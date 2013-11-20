7cd8d590aac537d7e2769110f027bef78c865f40
Bryan Ricker <bricker88@gmail.com>
2013-11-20 10:06:14 -0800

    Remove unnecessary require

--- config/initializers/secretary.rb
+++ config/initializers/secretary.rb
@@ -1,13 +1,7 @@
-# First configure, because Secretary::Version uses the confiration
 Secretary.configure do |config|
   config.user_class = "::AdminUser"
 end
 
-# Then require secretary/version so we know it's loaded before we
-# force-feed it a spoonful of Outpost.
-require 'secretary/version'
-
-# nom nom nom
 Secretary::Version.instance_eval do
   outpost_model
 end
