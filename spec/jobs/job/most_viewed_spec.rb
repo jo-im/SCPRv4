fb0a58e98c671b8e8205df3e7e7ed6786a6e8187
Bryan Ricker <bricker88@gmail.com>
2013-11-21 11:04:09 -0800

    Stub ContentBase#obj_by_url in most viewed spec

--- spec/jobs/job/most_viewed_spec.rb
+++ spec/jobs/job/most_viewed_spec.rb
@@ -10,6 +10,10 @@ describe Job::MostViewed do
       Job::MostViewed.any_instance.should_receive(:fetch).and_return(
         JSON.parse(load_fixture("api/google/analytics_most_viewed.json")))
 
+      ContentBase.should_receive(:obj_by_url)
+        .with("/news/2013/11/17/1/tornadoes-in-illinois-cause-severe-damage/")
+        .and_return(story)
+
       Job::MostViewed.perform
 
       popular = Rails.cache.read("popular/viewed")
