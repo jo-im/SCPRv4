8d7b97dc447fa7c33d1a09ed52ff3ea2015d200e
Bryan Ricker <bricker88@gmail.com>
2013-11-20 18:33:10 -0800

    Flesh out remote article factories

--- spec/factories/remote_articles.rb
+++ spec/factories/remote_articles.rb
@@ -4,9 +4,21 @@ FactoryGirl.define do
 
   factory :chr_article, class: "RemoteArticle" do
     source "chr"
+    headline "CHR Article"
+    teaser "This is a short thing about the article"
+    published_at { Time.now }
+    url "http://chr.org/wat.html"
+    article_id 12345
+    is_new true
   end
 
   factory :npr_article, class: "RemoteArticle" do
     source "npr"
+    headline "CHR Article"
+    teaser "This is a short thing about the article"
+    published_at { Time.now }
+    url "http://chr.org/wat.html"
+    article_id 12345
+    is_new true
   end
 end
