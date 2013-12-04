1de5ee54bff2a15821d1b95b6495c00d9ad9ff86
Jeremy Hoffing <jhoffing@scpr.org>
2013-12-04 15:30:37 -0800

    Only show issue articles that have been published

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -14,7 +14,7 @@ class IssuesController < ApplicationController
     @issues = Issue.active
     @issue = Issue.active.find_by_slug!(params[:slug])
     @issue_articles = @issue.articles
-    @paginated_articles = Kaminari.paginate_array(@issue.articles).page(params[:page]).per(8)
+    @paginated_articles = Kaminari.paginate_array(@issue_articles).page(params[:page]).per(8)
     @article_count = @issue.articles.count
     if Rails.cache.read("popular/viewed").presence
       @top_popular_articles = Rails.cache.read("popular/viewed")[0..1]
--- app/models/article_issue.rb
+++ app/models/article_issue.rb
@@ -1,5 +1,7 @@
 class ArticleIssue < ActiveRecord::Base
   belongs_to :issue
-  belongs_to :article, polymorphic: true
+  belongs_to :article,
+    polymorphic: true,
+    conditions: { status: ContentBase::STATUS_LIVE }
   # attr_accessible :title, :body
 end
