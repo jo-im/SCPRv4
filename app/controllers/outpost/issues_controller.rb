d66fef3ff47ab1539618fe3611bc32ef20264c01
Bryan Ricker <bricker88@gmail.com>
2013-12-04 09:28:41 -0800

    Cleanup outpost issues controller and specs [ci skip]

--- app/controllers/outpost/issues_controller.rb
+++ app/controllers/outpost/issues_controller.rb
@@ -2,17 +2,28 @@ class Outpost::IssuesController < Outpost::ResourceController
   outpost_controller
 
   define_list do |l|
-    l.default_order_attribute   = "title"
-    l.default_order_direction   = ASCENDING
+    l.default_order_attribute   = "created_at"
+    l.default_order_direction   = DESCENDING
 
-    l.column :title, sortable: true
-    l.column :slug, sortable: true
+    l.column :title,
+      :sortable => true,
+      :default_order_direction => ASCENDING
+
+    l.column :slug,
+      :sortable => true,
+      :default_order_direction => ASCENDING
+
     l.column :description
     l.column :is_active
 
-#    l.filter :is_news,
-#      :title        => "News?",
-#      :collection   => :boolean
+    l.column :created_at,
+      :sortable => true,
+      :default_order_direction => DESCENDING
+
+
+    l.filter :is_active,
+      :title        => "Active?",
+      :collection   => :boolean
   end
 end
 
