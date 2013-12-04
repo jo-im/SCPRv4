a6508a58913671e86f2364fe9b512780ec3ba412
Jeremy Hoffing <jhoffing@scpr.org>
2013-12-04 13:46:51 -0800

    Paginate individual issue page

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -13,7 +13,8 @@ class IssuesController < ApplicationController
   def show
     @issues = Issue.active
     @issue = Issue.active.find_by_slug!(params[:slug])
-    @issue_articles = @issue.articles.first(8)
+    @issue_articles = @issue.articles
+    @paginated_articles = Kaminari.paginate_array(@issue.articles).page(params[:page]).per(8)
     @article_count = @issue.articles.count
     if Rails.cache.read("popular/viewed").presence
       @top_popular_articles = Rails.cache.read("popular/viewed")[0..1]
