935c5abd90f0aa1e40cdafd087fb5d385a53325e
Bryan Ricker <bricker88@gmail.com>
2013-12-06 02:26:28 -0800

    Remove unused files for master

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,46 +25,6 @@ class BlogsController < ApplicationController
   def entry
     @entry = BlogEntry.published.includes(:blog).find(params[:id])
     @blog  = @entry.blog
-
-#    @entry = BlogEntry.published.includes(:blog).find(params[:id])
-#    @blog  = @entry.blog
-#    @asset = @entry.asset if @entry.asset.present?
-#    @related_articles = @entry.related_content.first(2) unless @entry.related_content.empty?
-#    @category = @entry.category
-#
-#    if @category.issues.any?
-#      @category_issues = @category.issues
-#      @special_issue = @category_issues.first
-#      @other_issues = @category_issues[1..2]
-#      @top_two_special_issue_articles ||= @special_issue.articles.first(2)
-#    end
-#
-#    page      = params[:page].to_i
-#    @content = @category.content(
-#      :page       => page,
-#      :per_page   => 11
-#    )
-#
-#    if @content.present?
-#      @category_articles = @content.map { |a| a.to_article }
-#    end
-#
-#    @popular_articles = Rails.cache.read("popular/viewed").first(3) if Rails.cache.read("popular/viewed").presence
-#
-#    if @category.featured_articles.any?
-#      @resources = @category.featured_articles[1..4]
-#    end
-#
-#    if @category.bios.any?
-#      @bios = @category.bios
-#      @twitter_feeds = @bios.map(&:twitter_handle)
-#    end
-#
-#    if @category.events.published.upcoming.any?
-#      @events = @category.events.published.upcoming.map(&:to_article)
-#    end
-#
-#    render layout: "vertical"
   end
 
   #----------
