634578a16b5f5950924a870be0a5116f6df6cfe3
Bryan Ricker <bricker88@gmail.com>
2013-12-05 21:37:22 -0800

    Add spec for verticals twitter cache job

--- spec/jobs/job/verticals_twitter_cache_spec.rb
+++ spec/jobs/job/verticals_twitter_cache_spec.rb
@@ -1,5 +1,20 @@
 require "spec_helper"
 
 describe Job::VerticalsTwitterCache do
-  pending
+  describe '::perform' do
+    it 'caches the tweet block' do
+      bio = create :bio, twitter_handle: "kpcc"
+      category = create :category, is_active: true, slug: "politics"
+      category.bios << bio
+
+      stub_request(:get, %r|twitter\.com|).to_return({
+        :body => load_fixture("api/twitter/user_timeline.json"),
+        :content_type => "application/json"
+      })
+
+      Rails.cache.read("verticals/#{category.slug}/twitter_feed").should be_nil
+      Job::VerticalsTwitterCache.perform
+      Rails.cache.read("verticals/#{category.slug}/twitter_feed").should_not be_nil
+    end
+  end
 end
