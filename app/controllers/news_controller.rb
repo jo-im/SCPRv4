0a5d7a7490fc83de53ac4bd3e09645d538a429a9
Jeremy Hoffing <jhoffing@scpr.org>
2013-12-10 14:06:21 -0800

    Refactor single page for news stories

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,49 +4,32 @@ class NewsController < NewApplicationController
 
   before_filter :get_popular_articles
 
+  PER_PAGE = 11
+
   def story
     @story = NewsStory.published.find_by_slug!(params[:slug])
-    @related_articles = @story.related_content.first(2) unless @story.related_content.empty?
 
-    @popular_articles = Rails.cache.read("popular/viewed").first(3) if Rails.cache.read("popular/viewed").presence
+    @news_category = @story.category
+
+    content_params = {
+      page:         params[:page].to_i,
+      per_page:     PER_PAGE
+    }
+
+    content_params[:exclude] = @story
+
 
     if @category = @story.category
-      if @category.issues.present?
-        @category_issues = @category.issues
-        @special_issue = @category_issues.first
-        @other_issues = @category_issues[1..2]
-        @top_two_special_issue_articles ||= @special_issue.articles.first(2)
-      end
-
-      page      = params[:page].to_i
-      @content = @category.content(
-        :page       => page,
-        :per_page   => 11
-      )
-
-      if @content.present?
-        @category_articles = @content.map { |a| a.to_article }
-        @three_recent_articles = @category_articles[0..2]
-        @more_articles = @category_articles[3..-1]
-      end
-
-      if @category.featured_articles.present?
-        @resources = @category.featured_articles[1..4]
-      end
-
-      if @category.bios.present?
-        @bios = @category.bios
-        @twitter_feeds = @bios.map(&:twitter_handle)
-      end
-
-      if @category.events.published.upcoming.present?
-        @events = @category.events.published.upcoming.map(&:to_article)
-      end
+      @featured_articles  = @category.featured_articles
+      @content = @category.content(content_params)
+      @category_articles = @content.map { |a| a.to_article }
+      @events  = @category.events.published.upcoming
     end
 
     if ( request.env['PATH_INFO'] =~ /\/\z/ ? request.env['PATH_INFO'] : "#{request.env['PATH_INFO']}/" ) != @story.public_path
       redirect_to @story.public_path and return
     end
+
     respond_with template: "news/story"
   end
 end
--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -1,54 +1,47 @@
 require "spec_helper"
 
 describe NewsController do
-  describe "GET /story" do
-    describe 'view' do
-      render_views
+  describe "story" do
+    render_views
 
-      it 'renders the view' do
-        story   = create :news_story
-        bio     = create :bio, twitter_handle: "bryanricker"
-        byline  = create :byline, content: story, user: bio
-
-        get :story, story.route_hash
-      end
+    before :each do
+      @story   = create :news_story
+      @bio     = create :bio, twitter_handle: "bryanricker"
+      @byline  = create :byline, content: @story, user: @bio
     end
 
-    describe 'controller' do
+    it 'renders the view' do
+      get :story, @story.route_hash
+    end
+
+    it "renders the layout" do
+      get :story, @story.route_hash
+      response.should render_template "new/single"
+    end
+
+    it "assigns @story" do
+      story = create :news_story, :published
+      get :story, story.route_hash
+      assigns(:story).should eq story
+    end
+
+    it "raises RecordNotFound if story not found" do
+      story = create :news_story, :published
+      -> {
+        get :story, { id: story.id, slug: 'awqweqweqwe' }.merge!(date_path(story.published_at))
+      }.should raise_error ActiveRecord::RecordNotFound
+    end
+
+    context "for popular articles" do
+      let(:articles) { create_list(:news_story, 3).map(&:to_article) }
+
       before :each do
-        @story   = create :news_story
-        @bio     = create :bio, twitter_handle: "bryanricker"
-        @byline  = create :byline, content: @story, user: @bio
-      end
-
-      it "renders the layout" do
+        Rails.cache.write("popular/viewed", articles)
         get :story, @story.route_hash
-        response.should render_template "layouts/vertical"
       end
 
-      it "assigns @story" do
-        story = create :news_story, :published
-        get :story, story.route_hash
-        assigns(:story).should eq story
-      end
-
-      it "raises RecordNotFound if story not found" do
-        story = create :news_story, :published
-        -> {
-          get :story, { id: "999999", slug: story.slug }.merge!(date_path(story.published_at))
-        }.should raise_error ActiveRecord::RecordNotFound
-      end
-      context "for popular articles" do
-        let(:articles) { create_list(:blog_entry, 3).map(&:to_article) }
-
-        before :each do
-          Rails.cache.write("popular/viewed", articles)
-          get :story, @story.route_hash
-        end
-
-        it 'assigns @popular_articles' do
-          assigns(:popular_articles).should eq articles
-        end
+      it 'assigns @popular_articles' do
+        assigns(:popular_articles).should eq articles
       end
     end
   end
