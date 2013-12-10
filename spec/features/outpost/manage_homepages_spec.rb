e728742d1a7b0d51a1a9af50e5644d8c998182c0
Bryan Ricker <bricker88@gmail.com>
2013-12-10 12:21:53 -0800

    Allow options for form fields to be passed into shared request specs

--- spec/features/outpost/manage_homepages_spec.rb
+++ spec/features/outpost/manage_homepages_spec.rb
@@ -1,12 +1,14 @@
 require "spec_helper"
 
 describe Homepage do
+  field_opts = { :field_options => { status: "status-select" } }
+
   let(:valid_record) { build :homepage, base: "default" }
   let(:invalid_record) { nil } # Can't make an invalid homepage in outpost.
   let(:updated_record) { build :homepage, base: "wide" }
 
-  it_behaves_like "managed resource"
+  it_behaves_like "managed resource", field_opts
   it_behaves_like "save options"
   it_behaves_like "admin routes"
-  it_behaves_like "versioned model"
+  it_behaves_like "versioned model", field_opts
 end
--- spec/features/outpost/manage_show_segments_spec.rb
+++ spec/features/outpost/manage_show_segments_spec.rb
@@ -1,13 +1,15 @@
 require "spec_helper"
 
 describe ShowSegment do
+  field_opts = { :field_options => { status: "status-select" } }
+
   let(:valid_record) { build :show_segment, :published }
   let(:updated_record) { build :show_segment, :published }
   let(:invalid_record) { build :show_segment, :published, headline: "" }
 
-  it_behaves_like "managed resource"
+  it_behaves_like "managed resource", field_opts
   it_behaves_like "save options"
   it_behaves_like "admin routes"
-  it_behaves_like "versioned model"
+  it_behaves_like "versioned model", field_opts
   it_behaves_like "front-end routes"
 end
