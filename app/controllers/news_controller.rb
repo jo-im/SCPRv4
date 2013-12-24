d46234b3a226346a0352a8a940a86748e8100496
Jeremy Hoffing <jhoffing@scpr.org>
2013-12-24 11:56:18 -0800

    Use new concern module for popular articles

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,5 @@
 class NewsController < NewApplicationController
-  include GetPopularArticles
+  include Concern::Controller::GetPopularArticles
   layout 'new/single'
   respond_to :html, :xml, :rss
 
