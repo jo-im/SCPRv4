ac1e3dfef359025c00bacaa31ebd7e1d22eb6968
Bryan Ricker <bricker88@gmail.com>
2014-02-10 20:09:32 -0800

    Cleanup [ci skip]

--- spec/features/vertical_spec.rb
+++ spec/features/vertical_spec.rb
@@ -23,30 +23,38 @@ describe "Vertical page" do
     end
 
     it "does not return the top story in the promoted issues section" do
-      issue = create :active_issue
-      category = create :category, is_active: true
-      category_issue = create :category_issue, category: category, issue: issue
-      featured_story = create :news_story, :published, category: category
+      issue           = create :active_issue
+      category        = create :category, is_active: true
+      featured_story  = create :news_story, :published, category: category
+
+      category.category_issues.create(issue: issue)
+      featured_story.article_issues.create(issue: issue)
       category.category_articles.create(article: featured_story)
-      other_articles = create_list :news_story, 10, :published
-      #assign issue to featured article and other articles
-      featured_article_issue = create :article_issue, article: featured_story, issue: issue
+
+      other_articles = create_list :news_story, 2, :published
+
+      # assign issue to featured article and other articles
       other_articles.each do |article|
-        create :article_issue, article: article, issue: issue
+        article.article_issues.create(issue: issue)
       end
+
       index_sphinx
+
       ts_retry(2) do
         visit category.public_path
+
         within("aside.more") do
-          #make sure top story doesn't show up in the promoted 'more from this issue' section
+          # make sure top story doesn't show up in the promoted 'more from this issue' section
           page.should_not have_content category.featured_articles.first.short_title
         end
+
         within("section.issues") do
-          #make sure top story will still show up in the Issues We're Tracking section
+          # make sure top story will still show up in the Issues We're Tracking section
           page.should have_content category.featured_articles.first.short_title, count: 1
         end
       end
     end
+
     # This spec is here becase an error occurred when a content shell
     # without issues was the lead article, since content shells don't have
     # related content.
