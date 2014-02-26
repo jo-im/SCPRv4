4865861b6a0a1322dd3a79c647fa4bb7711329ab
Bryan Ricker <bricker88@gmail.com>
2014-02-26 00:24:42 -0800

    Add note

--- config/initializers/json_escape.rb
+++ config/initializers/json_escape.rb
@@ -1,5 +1,6 @@
 # Fix json_escape to properly escape
 # https://github.com/rails/rails/pull/6094
+# This monkey patch can be removed for Rails 4.1
 class ActionView::Base
   def json_escape(s)
     result = s.to_s.gsub('/', '\/')
