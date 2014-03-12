d300346742aa58c695ae5c75b3f3691b590807b9
Bryan Ricker <bricker88@gmail.com>
2014-03-12 15:50:56 -0700

    Fix stub regex

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,7 +97,7 @@ RSpec.configure do |config|
       :content_type => "application/json"
     })
 
-    stub_request(:get, %r{scpr\.org/.+/\.mp3\z}).to_return({
+    stub_request(:get, %r|scpr\.org/.+\.mp3\z|).to_return({
       :content_type => 'audio/mpeg',
       :body         => load_fixture('media/audio/2sec.mp3')
     })
