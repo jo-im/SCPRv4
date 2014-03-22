9620c7bdc04b69255c5e81304c7ead25c9505dc3
Bryan Ricker <bricker88@gmail.com>
2014-03-21 21:25:34 -0700

    Switch back to secret_token config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,5 @@
-Scprv4::Application.config.secret_key_base =
+# Scprv4::Application.config.secret_key_base =
+#   Rails.application.config.secrets['secret_token']
+
+Scprv4::Application.config.secret_token =
   Rails.application.config.secrets['secret_token']
