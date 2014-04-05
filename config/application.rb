7e9b76b2a8cd5ee68b831ed36e8ad9ceeb339b2a
Sean Dillingham <sean.dillingham@gmail.com>
2014-04-04 19:08:01 -0700

    remove duplicate print.css reference in asset precompile list

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module Scprv4
       "outpost/application.css",
       "outpost/application.js",
       "base/print.css",
-      "base/print.css",
       "new/application.js",
       "new/application.css",
       "new/ie-lt9.js",  # For ie < 9, separate include
