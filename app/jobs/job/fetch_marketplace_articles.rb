b23fca08b405a9c47a8252c4a0514a96af763044
Jeremy Hoffing <jhoffing@scpr.org>
2014-04-30 12:49:43 -0700

    Fetch marketplace RSS feed and cache HTML articles

---
+++ app/jobs/job/fetch_marketplace_articles.rb
@@ -0,0 +1,39 @@
+require 'rss'
+
+module Job
+  class FetchMarketplaceArticles < Base
+    @priority = :low
+
+    RSS_URL   = "http://www.marketplace.org/latest-stories/long-feed.xml"
+    LIMIT     = 2
+
+    class << self
+      def perform
+        feed = RSS::Parser.parse(RSS_URL, false)
+
+        log "#{feed.items.size} marketplace stories found"
+
+
+        if !feed || feed.items.empty?
+          log "Feed is empty. Aborting. (#{ENDPOINT})"
+          return false
+        end
+
+        latest_articles = feed.items.first(LIMIT)
+        Rails.cache.write("business/marketplace", latest_articles)
+
+        self.cache(latest_articles,
+          "/shared/widgets/cached/marketplace",
+          "views/business/marketplace",
+          local: :articles
+        )
+
+        latest_articles.each do |article|
+          log "Wrote Marketplace Story #{article.link} to the cache"
+        end
+      end
+
+      #---------------
+    end
+  end # MostViewed
+end # Job
---
+++ spec/jobs/job/fetch_marketplace_articles_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'rss'
+describe Job::FetchMarketplaceArticles do
+  subject { described_class }
+  its(:queue) { should eq "scprv4:low_priority" }
+
+  before :each do
+    stub_request(:get, %r{http://www.marketplace.org/latest-stories/long-feed.xml}).to_return({
+      :content_type   => 'text/xml',
+      :body           => load_fixture('marketplace.xml')
+    })
+  end
+  describe '::perform' do
+    it "fetches and parses the Marketplace RSS feed, then writes to cache" do
+      Job::FetchMarketplaceArticles.perform
+
+      marketplace_articles = Rails.cache.read("business/marketplace")
+      marketplace_articles.should_not be_nil
+    end
+  end
+end
+
