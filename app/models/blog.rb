eb313522532a3e59e58b4688611c0023cf38cc73
Bryan Ricker <bricker88@gmail.com>
2014-05-05 17:26:02 -0700

    Order blog collections with active blogs at the top

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -31,7 +31,7 @@ class Blog < ActiveRecord::Base
     # Maps all records to an array of arrays, to be
     # passed into a Rails select helper
     def select_collection
-      Blog.order("name").map { |blog| [blog.to_title, blog.id] }
+      Blog.order("is_active desc, name").map { |blog| [blog.to_title, blog.id] }
     end
   end
 
