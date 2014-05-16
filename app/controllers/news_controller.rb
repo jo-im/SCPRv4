a886516f48f8e298fad3ba56dc982ac6541ffb0a
Bryan Ricker <bricker88@gmail.com>
2014-05-16 13:48:02 -0700

    Revert "Revert "Temporary cache 44167""
    This reverts commit 8a0681ef777bb05c0a1c46783dd66f4ad7e0983b.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,6 @@
 class NewsController < ApplicationController
+  caches_action :story, if: -> { params[:id].to_i == 44167 }, expires_in: 10.minutes
+
   def story
     @story = NewsStory.published.find(params[:id])
 
