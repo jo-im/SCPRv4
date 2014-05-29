9d8292b77dd1d3c3611a28ae60a72d2e147ae8b9
Bryan Ricker <bricker88@gmail.com>
2014-05-28 23:08:24 -0700

    Touch tags on publish

--- app/concerns/concern/associations/tags_association.rb
+++ app/concerns/concern/associations/tags_association.rb
@@ -10,6 +10,25 @@ module Concern
         if self.has_secretary?
           tracks_association :tags
         end
+
+        after_save :touch_tags, if: :should_touch_tags?
+      end
+
+
+      private
+
+      # Since this module is shared across several models,
+      # we only want to touch the tags if the object is publishable
+      # and is currently being published.
+      def should_touch_tags?
+        self.respond_to?(:publishing?) && self.publishing?
+      end
+
+      # Touch the tags so we can use their "updated_at" data
+      # to populate the "coverage began/ended" metadata on the
+      # Issues pages.
+      def touch_tags
+        self.tags.each(&:touch)
       end
     end
   end
--- spec/concerns/concern/associations/tags_association_spec.rb
+++ spec/concerns/concern/associations/tags_association_spec.rb
@@ -5,4 +5,17 @@ describe Concern::Associations::TagsAssociation do
 
   it { should have_many :taggings }
   it { should have_many :tags }
+
+  it "touches tags after save when publishing" do
+    tag = create :tag
+    tag.update_column(:updated_at, 1.month.ago)
+
+    tag.reload.updated_at.should be < 10.minutes.ago
+
+    story = build :test_class_story
+    story.tags << tag
+    story.save!
+
+    tag.reload.updated_at.should be > 10.minutes.ago
+  end
 end
