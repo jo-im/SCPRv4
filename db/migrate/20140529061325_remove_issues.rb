087e8220ae957d78e2013b257664f3e9eecbb19e
Bryan Ricker <bricker88@gmail.com>
2014-05-30 14:24:53 -0700

    Remove destructive migrations; schema update

--- db/migrate/20140529061325_remove_issues.rb
+++
@@ -1,7 +0,0 @@
-class RemoveIssues < ActiveRecord::Migration
-  def change
-    drop_table :issues
-    drop_table :article_issues
-    drop_table :category_issues
-  end
-end
--- db/migrate/20140529062114_remove_issue_permission.rb
+++
@@ -1,7 +0,0 @@
-class RemoveIssuePermission < ActiveRecord::Migration
-  def change
-    p = Permission.where(resource: "Issue").first
-    UserPermission.where(permission_id: p.id).destroy_all
-    p.delete
-  end
-end
