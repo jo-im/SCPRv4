25647934cc014384b2939c5648793750cd81afa4
Bryan Ricker <bricker88@gmail.com>
2014-06-03 13:43:59 -0700

    Uncomment missing code

--- app/controllers/outpost/home_controller.rb
+++ app/controllers/outpost/home_controller.rb
@@ -3,9 +3,9 @@ class Outpost::HomeController < Outpost::BaseController
     breadcrumb "Dashboard", outpost.root_path
 
     # Get the latest activity
-    # @current_user_activities = current_user.activities
-    #   .where("created_at > ?", 1.week.ago)
-    #   .order("created_at desc").limit(10)
+    @current_user_activities = current_user.activities
+      .where("created_at > ?", 1.week.ago)
+      .order("created_at desc").limit(10)
 
     @latest_activities = Secretary::Version.order("created_at desc")
       .where("user_id != ?", current_user.id).limit(10)
