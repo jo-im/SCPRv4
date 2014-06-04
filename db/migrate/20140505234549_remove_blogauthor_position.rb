1a45441fa89b0b7cc492c9a146bb8d122598d5c3
Bryan Ricker <bricker88@gmail.com>
2014-06-04 11:30:50 -0700

    Remove old files

--- db/migrate/20140505234549_remove_blogauthor_position.rb
+++
@@ -1,9 +0,0 @@
-class RemoveBlogauthorPosition < ActiveRecord::Migration
-  def up
-    remove_column :blogs_blogauthor, :position
-  end
-
-  def down
-    add_column :blogs_blogauthor, :position, :integer
-  end
-end
