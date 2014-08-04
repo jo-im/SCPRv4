c1d38a0994c0a62f5ad09554944bee44dfeb0705
Jon White <mr.jon.white@gmail.com>
2014-08-04 16:55:48 -0700

    Begun the work in earnest to responsify Short List

--- app/assets/javascripts/new/editions.js
+++ app/assets/javascripts/new/editions.js
@@ -2,16 +2,40 @@ scpr.Behaviors.Editions = {
 
     loadBehaviors: function() {
       
-
+      // Give the OL back its numbers.
+      // ---------------------------------------------------------
       $(".contents ol li").each(function(){
         var myIndex = $(this).index() + 1;
         $(this).find("a").prepend("<mark>" + myIndex + "</mark> ");
       });
       
       
+      // Show/hide the "Recent Editions" flyout.
+      // ---------------------------------------------------------
       $(".edition-marquee time mark,.recents button").click(function(){
         $(".edition-marquee").toggleClass("exposed");
       });
+
+      
+      // Shorten cosmetic names of days-of-the-week. (Debounced.)
+      // ---------------------------------------------------------
+      function dayShortener() {
+          if ($(window).width() < 761 ){
+            $(".recents li").each(function(){
+              var shortName = $(this).find("time span").attr("data-short");
+              $(this).find("time span").html(shortName);
+            });
+          } else {
+            $(".recents li").each(function(){
+              var longName = $(this).find("time span").attr("data-long");
+              $(this).find("time span").html(longName);
+            });
+          }
+      };
+      $(window).resize( $.debounce( 250, dayShortener ) );
+      
+
+
 
 
     } // loadBehaviors
