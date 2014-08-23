6711ad7d7a538a8e3c1962d9df9a9356e25c94df
Jeremy Hoffing <jhoffing@scpr.org>
2014-08-23 01:40:07 -0700

    Add latest page and routes for short-list/latest

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -2,8 +2,11 @@ class EditionsController < ApplicationController
   layout 'new/ronin'
   respond_to :html
 
-  def index
-    @edition = Edition.published.includes(:slots).first
+  def latest
+    @scoped_editions = Edition.published.includes(:slots)
+    @edition = @scoped_editions.first
+    @other_editions = @scoped_editions.where.not(id: @edition).first(4)
+    render template: "editions/short_list"
   end
 
   def short_list
