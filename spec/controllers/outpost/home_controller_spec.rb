16632d55164ba48a159bb8cde55f4dae74820410
Bryan Ricker <bricker88@gmail.com>
2014-09-09 23:14:57 -0700

    Add global search to Outpost

--- spec/controllers/outpost/home_controller_spec.rb
+++ spec/controllers/outpost/home_controller_spec.rb
@@ -2,13 +2,14 @@ require "spec_helper"
 
 describe Outpost::HomeController do
   render_views
-  routes { Outpost::Engine.routes }
+
+  before do
+    @current_user = create :admin_user
+    controller.stub(:current_user) { @current_user }
+  end
 
   describe 'GET dashboard' do
-    before :each do
-      @current_user = create :admin_user
-      controller.stub(:current_user) { @current_user }
-    end
+    routes { Outpost::Engine.routes }
 
     it 'gets the current user activities' do
       story = create :news_story, logged_user_id: @current_user.id
@@ -25,6 +26,44 @@ describe Outpost::HomeController do
 
       get :dashboard
       assigns(:latest_activities).should eq story2.versions.to_a
+    end
+  end
+
+  describe 'GET search' do
+    routes { Rails.application.routes }
+
+    sphinx_spec
+
+    it 'gets the records from the query' do
+      ns = create :news_story, headline: "Obama"
+      be = create :blog_entry, headline: "President Obama"
+      pq = create :pij_query, headline: "Something about Obama"
+
+      index_sphinx
+
+      ts_retry(2) do
+        get :search, gquery: "Obama"
+
+        records = assigns(:records)
+        expect(records).to include ns
+        expect(records).to include be
+        expect(records).to include pq
+      end
+    end
+
+    it 'only searches among models with an index' do
+      vertical = create :vertical, title: "Obama"
+      be = create :blog_entry, headline: "President Obama"
+
+      index_sphinx
+
+      ts_retry(2) do
+        get :search, gquery: "Obama"
+
+        records = assigns(:records)
+        expect(records).to include be
+        expect(records).not_to include vertical
+      end
     end
   end
 end
--- spec/features/outpost/search_spec.rb
+++ spec/features/outpost/search_spec.rb
@@ -12,7 +12,7 @@ describe "Search" do
     page.should have_content story.headline
     page.should have_content story2.headline
 
-    within 'form.form-search' do
+    within '.index-header .form-search' do
       fill_in "query", with: "Poppins"
       find('button').click
     end
@@ -32,12 +32,79 @@ describe "Search" do
     page.should have_content story.headline
     page.should have_content story2.headline
 
-    within 'form.form-search' do
+    within '.index-header .form-search' do
       fill_in "query", with: "$800"
       find('button').click
     end
 
     page.should have_content story.headline
     page.should_not have_content story2.headline
+  end
+
+  describe 'global search' do
+    it 'shows the search results' do
+      ns = create :news_story, headline: "Obama"
+      be = create :blog_entry, headline: "President Obama"
+      pq = create :pij_query, headline: "Something about Obama"
+
+      index_sphinx
+
+      login
+
+      within ".form-search" do
+        fill_in "gquery", with: "Obama"
+        find('button').click
+      end
+
+      expect(page).to have_content ns.headline
+      expect(page).to have_content be.headline
+      expect(page).to have_content pq.headline
+    end
+
+    it 'shows a message if no results are found' do
+      login
+
+      within ".form-search" do
+        fill_in "gquery", with: "Supercalifragilisticexpialidocious"
+        find('button').click
+      end
+
+      expect(page).to have_content "No Results"
+    end
+
+    it 'paginates' do
+      create_list :news_story, 30, headline: "Obama"
+      index_sphinx
+
+      login
+
+      within ".form-search" do
+        fill_in "gquery", with: "Obama"
+        find('button').click
+      end
+
+      within ".pagination" do
+        expect(page).to have_link "2"
+        click_link "2"
+      end
+
+      within ".pagination" do
+        expect(page).to have_link "1"
+      end
+    end
+
+    it "doesn't show records that can't be edited" do
+      ra = create :remote_article, headline: "xxObama Remote Article Headline--"
+      index_sphinx
+
+      login
+
+      within ".form-search" do
+        fill_in "gquery", with: "Obama"
+        find('button').click
+      end
+
+      expect(page).not_to have_content ra.headline
+    end
   end
 end
