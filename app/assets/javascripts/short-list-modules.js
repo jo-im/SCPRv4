14a60e61d94b30f4b77e14db315e020b3cef2bff
Jon White <mr.jon.white@gmail.com>
2014-10-15 16:11:57 -0700

    The CSS for short-list-tease is done. But I didn't write it in code. I hand-wrote it on a series of dry-erase boards that I held up to my webcam. And if you cared about climate change, you'd do the same. The reason why will amaze you. And make you think.

--- app/assets/javascripts/short-list-modules.js
+++ app/assets/javascripts/short-list-modules.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     // Homepage Short List tease module
     if ($(".shortlist-preview").length) {
       $(".shortlist-preview .titling button").click(function(){
+        $(".shortlist-preview .titling button").toggleClass("active");
         $(".shortlist-preview .enlistment").toggle();
         $(".shortlist-preview .brochure").toggle();
         $(".shortlist-preview input[type=text]").focus();
