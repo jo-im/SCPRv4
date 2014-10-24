a5c499214a2a040ca498a9d3bebea3423701c80a
Sean Dillingham <sean.dillingham@gmail.com>
2014-10-23 22:16:32 -0700

    more WIP on homepage Short List module

--- app/assets/javascripts/short-list-modules.js
+++ app/assets/javascripts/short-list-modules.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     if ($(".shortlist-preview").length) {
       $(".shortlist-preview .titling button").click(function(){
         $(".shortlist-preview .titling button").toggleClass("active");
+        ($(".shortlist-preview .titling button span").text() === "Subscribe") ? $(".shortlist-preview .titling button span").text("Close") : $(".shortlist-preview .titling button span").text("Subscribe");
         $(".shortlist-preview .enlistment").toggle();
         $(".shortlist-preview .brochure").toggle();
         $(".shortlist-preview input[type=text]").focus();
