6b10bc92092be264d7ae894ae3e4c40b72decb73
Sean Dillingham <sean.dillingham@gmail.com>
2014-12-02 21:26:50 -0800

    update generate_body_callback spec to use segment

--- spec/concerns/concern/callbacks/generate_body_callback_spec.rb
+++ spec/concerns/concern/callbacks/generate_body_callback_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe Concern::Callbacks::GenerateBodyCallback do
   describe '#generate_body' do
-    it "runs before validation if article body is blank" do
-      story = build :test_class_story, body: nil, teaser: "Hello"
-      story.save!
-      story.body.should eq "Hello"
+    it "runs before validation if segment body is blank" do
+      segment = build :show_segment, body: nil, teaser: "Hello"
+      segment.save!
+      segment.body.should eq "Hello"
     end
 
     it "doesn't run if body is present" do
-      story = build :test_class_story, body: "Okay", teaser: "Okedoke"
-      story.save!
-      story.body.should eq "Okay"
+      segment = build :show_segment, body: "Okay", teaser: "Okedoke"
+      segment.save!
+      segment.body.should eq "Okay"
     end
   end
 end
