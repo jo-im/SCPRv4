2386fb0d0e9405048d54a2dc671c59d456a6d38f
Eric Richardson <e@ericrichardson.com>
2015-01-26 15:48:59 -0500

    Tests: Fix specs around PublishNotifications

--- spec/jobs/job/publish_notification_spec.rb
+++ spec/jobs/job/publish_notification_spec.rb
@@ -3,16 +3,4 @@ require 'spec_helper'
 describe Job::PublishNotification do
   subject { described_class }
   it { subject.queue.should eq Job::QUEUES[:low_priority] }
-
-  it "sends the message to campfire" do
-    # This is a useless test
-    room = double
-    campfire = double
-
-    Tinder::Campfire.should_receive(:new).and_return(campfire)
-    campfire.should_receive(:find_room_by_id).and_return(room)
-    room.should_receive(:speak)
-
-    Job::PublishNotification.perform("cool message", "web_dev")
-  end
 end
