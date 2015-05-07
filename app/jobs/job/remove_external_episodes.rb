512372afbfffe872558b53a075b795647bba6f78
Ben Titcomb <btitcomb@scpr.org>
2015-05-07 15:10:34 -0700

    Add logging to RemoveExternalEpisodes job.

--- app/jobs/job/remove_external_episodes.rb
+++ app/jobs/job/remove_external_episodes.rb
@@ -4,9 +4,11 @@ module Job
 
     class << self
       def perform
-				ExternalProgram.where('days_to_expiry IS NOT NULL').each do |program|
-					program.expired_episodes.destroy_all
-				end
+        logger.info "Sweeping expired episodes form external programs. - #{Time.now}"
+        ExternalProgram.where('days_to_expiry IS NOT NULL').each do |program|
+          program.expired_episodes.destroy_all
+        end
+        logger.info "Finished sweeping expired external episodes. - #{Time.now}"
       end
     end
   end
