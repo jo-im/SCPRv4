58ed1bc01f9c1cc29ac5cc5ff2444fd4ed9d1e7d
Ben Titcomb <btitcomb@scpr.org>
2015-05-13 11:03:55 -0700

    Change PopulateTagCoverageTimestamps migration to use an ES query.

--- db/migrate/20150511200111_populate_tag_coverage_timestamps.rb
+++ db/migrate/20150511200111_populate_tag_coverage_timestamps.rb
@@ -2,12 +2,12 @@ class PopulateTagCoverageTimestamps < ActiveRecord::Migration
   def up
     Tag.reset_column_information
     Tag.all.each do |tag|
-      articles = tag.articles
-      tag.update began_at: articles.last.try(:public_datetime), most_recent_at: articles.first.try(:public_datetime)  
+      oldest_article_timestamp  = tag.articles(order:"public_datetime asc",limit:1).first.try(:public_datetime)
+      newest_article_timestamp  = tag.articles(order:"public_datetime desc",limit:1).first.try(:public_datetime)
+      tag.update began_at: oldest_article_timestamp, most_recent_at: newest_article_timestamp
     end
   end
   def down
-    Tag.reset_column_information
     Tag.update_all began_at: nil, most_recent_at: nil
   end
 end
