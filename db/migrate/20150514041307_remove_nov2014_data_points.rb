beab418fead000a3a0df46ccb8fb214c0157fb58
Sean Dillingham <sean.dillingham@gmail.com>
2015-05-21 08:14:04 -0700

    revert to 62364127fb2e506a582febcf752faab93aa1cabc

--- db/migrate/20150514041307_remove_nov2014_data_points.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNov2014DataPoints < ActiveRecord::Migration
-  def up
-    DataPoint.delete_all :group_name => 'election-nov2014'
-  end
-
-  def down
-    # there is no going back.
-  end
-end
