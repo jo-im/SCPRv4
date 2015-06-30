631eb0e4653060f81f750d7d2589160bf215ee4f
Ben Titcomb <btitcomb@scpr.org>
2015-06-30 13:36:43 -0700

    Remove old commends out of content sanitizer spec.

--- spec/concerns/concern/sanitizers/content_spec.rb
+++ spec/concerns/concern/sanitizers/content_spec.rb
@@ -1,15 +1,8 @@
 require "spec_helper"
 
 describe Concern::Sanitizers::Content do
-  # Don't use a factory here because it fills in attributes that we don't want
-  # it to. We don't want the Generate* callbacks to be run.
+
   subject { 
-    # fake = Class.new
-    # fake.class_eval{
-    #   attr_accessor :body
-    #   include Concern::Sanitizers::Content
-    # }
-    # fake.new
     NewsStory.class_eval{
       attr_accessor :body
       include Concern::Sanitizers::Content
@@ -19,6 +12,7 @@ describe Concern::Sanitizers::Content do
 
   context "body of subject contains line break and paragraph break unicode characters" do
     it "removes those characters" do
+      # the string being assigned below contains both a line break character and paragraph break character
       subject.body = "linebreak paragraphbreak "
       subject.save
       expect(subject.body).to eq "linebreakparagraphbreak"
