6dd8e4f5ddf870c1b3c1099c2c55fcfc51e95088
Ben Titcomb <btitcomb@scpr.org>
2015-08-12 14:56:46 -0700

    Test #publish_email method on eloqua_email.

--- spec/models/eloqua_email_spec.rb
+++ spec/models/eloqua_email_spec.rb
@@ -24,8 +24,44 @@ describe EloquaEmail do
         edition = build :edition, :with_abstract
         edition.save
         eloqua_email = edition.eloqua_emails.last
-        # eloqua_email = build :eloqua_email, html_template: "/editions/email/template"
         eloqua_email.html_body.should match /<html/
+      end
+
+      it "renders an plain text body" do
+        edition = build :edition, :with_abstract
+        edition.save
+        eloqua_email = edition.eloqua_emails.last
+        eloqua_email.plain_text_body.should be_a String
+      end
+    end
+
+    describe "#publish_email" do
+      before do
+        stub_request(:post, %r|assets/email|).to_return({
+          :content_type   => "application/json",
+          :body           => load_fixture("api/eloqua/email.json")
+        })
+
+        stub_request(:post, %r|assets/campaign/active|).to_return({
+          :content_type   => "application/json",
+          :body           => load_fixture("api/eloqua/campaign_activated.json")
+        })
+
+        stub_request(:post, %r|assets/campaign\z|).to_return({
+          :content_type   => "application/json",
+          :body           => load_fixture("api/eloqua/email.json")
+        })
+
+        # Just incase, we don't want this method queueing anything
+        # since we're testing the publish method directly.
+        Edition.any_instance.stub(:async_send_email)
+      end
+      it "sets email_sent to true" do
+        edition = build :edition, :with_abstract
+        edition.save
+        eloqua_email = edition.eloqua_emails.last
+        eloqua_email.publish_email
+        eloqua_email.email_sent.should be true
       end
     end
 
