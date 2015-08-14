b1509e66a1838c0deda2fbae28ec6c1c85802b01
Ben Titcomb <btitcomb@scpr.org>
2015-08-14 12:30:20 -0700

    Try out newrelic#notice_error options.

--- app/jobs/job/twitter_cache.rb
+++ app/jobs/job/twitter_cache.rb
@@ -97,7 +97,7 @@ module Job
         tweets = @tweeter.user_timeline(@screen_name, twitter_options)
         options[:count] ? tweets.first(options[:count]) : tweets
       rescue Twitter::Error::NotFound => e
-        NewRelic.log_error(e)
+        NewRelic.log_error(e, {custom_params: {handle: @screen_name}})
         warn "Error caught in TwitterCache#fetch: #{e}"
         self.log "Error: \n #{e}"
         false
