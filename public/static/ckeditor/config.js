86253b546fb18eb1d3ac4d78c577ce361b61170f
Ben Titcomb <btitcomb@scpr.org>
2015-12-01 14:07:04 -0800

    Add format selector to ckeditor.

--- public/static/ckeditor/config.js
+++ public/static/ckeditor/config.js
@@ -3,14 +3,14 @@ CKEDITOR.timestamp = '20140219';
 CKEDITOR.editorConfig = function(config) {
 
   config.toolbar = [
-    ['Bold', 'Italic', 'Underline', "RemoveFormat"],
+    ['Format', 'Bold', 'Italic', 'Underline', "RemoveFormat"],
     ['NumberedList', 'BulletedList', 'Blockquote'],
     ['Link', 'Unlink', 'DockAssetManager', 'EmbedPlaceholder'],
     ['Find', 'Paste'],
     ['Source', 'Maximize']
   ];
 
-  config.format_tags = 'p;h2;h3';
+  config.format_tags = 'p;h1;h2;h3';
 
   config.extraPlugins = [
     'image', // This plugin has custom changes so we didn't include it in the build
