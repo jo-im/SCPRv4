1d6a5ec12caa063731da4af7f3104c09ccf749e6
Ben Titcomb <btitcomb@scpr.org>
2015-12-01 12:59:26 -0800

    Display tags by category in outpost form input.

--- db/migrate/20151120004231_add_image_and_type_to_tags.rb
+++ db/migrate/20151120004231_add_image_and_type_to_tags.rb
@@ -1,6 +1,6 @@
 class AddImageAndTypeToTags < ActiveRecord::Migration
   def change
     add_column :tags, :image, :string
-    add_column :tags, :tag_type, :string, default: "keyword", index: true
+    add_column :tags, :tag_type, :string, default: "Keyword", index: true
   end
 end
