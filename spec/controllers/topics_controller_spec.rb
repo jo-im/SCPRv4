7abc6227966f71ff608cf9b544f3402b2a087344
Ben Titcomb <btitcomb@scpr.org>
2015-12-08 11:13:13 -0800

    Remove old tests from topics controller spec.

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -3,24 +3,6 @@ require 'spec_helper'
 describe TopicsController, :indexing do
   render_views
 
-  describe 'GET /topics' do
-    it 'sets @tags to all active tags' do
-      tags = create_list :tag, 3
-
-      get :index
-
-      assigns(:tags).should eq tags.sort_by(&:title)
-    end
-
-    it "assigns popular articles" do
-      article = create(:news_story).to_article
-      Rails.cache.write("popular/viewed", [article])
-
-      get :index
-      assigns(:popular_articles).should eq [article]
-    end
-  end
-
   describe 'GET show' do
     it 'sets tag' do
       tag = create :tag
@@ -42,15 +24,6 @@ describe TopicsController, :indexing do
       tag = create :tag
       get :show, slug: tag.slug
       assigns(:tag).should eq tag
-    end
-
-    it "assigns popular articles" do
-      article = create(:news_story).to_article
-      Rails.cache.write("popular/viewed", [article])
-
-      tag = create :tag
-      get :show, slug: tag.slug
-      assigns(:popular_articles).should eq [article]
     end
 
     it "raises an error if the slug isn't found" do
