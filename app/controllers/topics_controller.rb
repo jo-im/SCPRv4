96d7d73bdfcf4f6709363944ea8aef83fc15440f
Ben Titcomb <btitcomb@scpr.org>
2015-12-08 10:31:10 -0800

    Clean up topics_controller.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,23 +2,11 @@ class TopicsController < ApplicationController
   layout 'new/ronin'
   respond_to :html, :xml, :rss
 
-  before_filter :get_popular_articles
-  before_filter :get_topics
-
   PER_PAGE = 8
-
-  def index
-  end
 
   def show
     @tag = Tag.find_by_slug!(params[:slug])
     @articles = @tag.articles( page:params[:page], per_page:PER_PAGE )
   end
 
-
-  private
-
-  def get_topics
-    @tags = Tag.order("title")
-  end
 end
