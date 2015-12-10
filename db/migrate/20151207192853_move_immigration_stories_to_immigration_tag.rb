8f60bc3db61956ff2164b5cd816a58f31bdc7bf0
Ben Titcomb <btitcomb@scpr.org>
2015-12-10 11:16:59 -0800

    Change immigration tag migration to account for existing immigration reform tag.

--- db/migrate/20151207192853_move_immigration_stories_to_immigration_tag.rb
+++ db/migrate/20151207192853_move_immigration_stories_to_immigration_tag.rb
@@ -1,20 +1,31 @@
 class MoveImmigrationStoriesToImmigrationTag < ActiveRecord::Migration
+  CONTENT_TYPES = ["NewsStory", "ContentShell", "BlogEntry", "ShowSegment", "Event", "Podcast", "Abstract", "Vertical"]
   def up
     category = Category.where(slug: 'immigration').first!
-    story_ids = NewsStory.where(category_id: category.id).pluck(:id)
-    shell_ids = ContentShell.where(category_id: category.id).pluck(:id)
-    tag_id    = Tag.where(slug: "immigration-3-0")
-    story_ids.each do |story_id|
-      Tagging.create taggable_id: story_id, taggable_type: "NewsStory", tag_id: tag_id
-    end
-    shell_ids.each do |shell_id|
-      Tagging.create taggable_id: shell_id, taggable_type: "ContentShell", tag_id: tag_id
+    tag_id   = Tag.where(slug: "immigration").first!.id
+    CONTENT_TYPES.each do |content_type|
+      content_type.constantize.where(category_id: category.id).find_in_batches do |batch|
+        batch.map(&:id).each do |content_id|
+          Tagging.create taggable_id: content_id, taggable_type: content_type, tag_id: tag_id
+        end
+      end
     end
     category.destroy
   end
   def down
-    Category.create title: "Immigration & Emerging Communities", slug: "immigration"
-    tag_id = Tag.where(slug: "immigration-3-0")
-    Tagging.where(tag_id: tag_id, taggable_type: ["NewsStory", "ContentShell"]).destroy_all
+    category = Category.create title: "Immigration & Emerging Communities", slug: "immigration"
+    tag_id   = Tag.where(slug: "immigration").first!.id
+    CONTENT_TYPES.each do |content_type|
+      table_name = content_type.constantize.table_name.to_sym
+      unless column_exists?(table_name, :category_id)
+        add_column table_name, :category_id, :integer, index: true
+      end
+    end
+    Tagging.where(tag_id: tag_id, taggable_type: CONTENT_TYPES).each do |tagging|
+      if taggable = tagging.taggable
+        taggable.update category_id: category.id
+      end
+      tagging.destroy
+    end
   end
 end
