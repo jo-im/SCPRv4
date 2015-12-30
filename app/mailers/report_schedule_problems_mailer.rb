0dd93f61ab7438754efc49497f3744523399f471
Ben Titcomb <btitcomb@scpr.org>
2015-12-30 09:04:44 -0800

    Use getter method for emails hash.

--- app/mailers/report_schedule_problems_mailer.rb
+++ app/mailers/report_schedule_problems_mailer.rb
@@ -2,7 +2,7 @@ class ReportScheduleProblemsMailer < ApplicationMailer
   def send_notification gaps, overlaps
     @gaps     = gaps
     @overlaps = overlaps
-    mail(to: Rails.application.secrets.emails.digital_broadcast_ops,
+    mail(to: Rails.application.secrets.emails["digital_broadcast_ops"],
          subject: "There is a problem with the KPCC program schedule")
   end
 end
