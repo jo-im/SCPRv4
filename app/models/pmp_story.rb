577066ce1add72dec95efeae34fa50695826cc40
Ben Titcomb <btitcomb@scpr.org>
2016-01-11 15:10:55 -0800

    Fix relations.

--- app/models/pmp_story.rb
+++ app/models/pmp_story.rb
@@ -1,8 +1,8 @@
 class PmpStory < PmpContent
   self.table_name = "pmp_contents"
   default_scope ->{where("pmp_contents.profile = 'story'")}
-  has_many :pmp_audios, dependent: :destroy
-  has_many :pmp_images, dependent: :destroy
+  has_many :pmp_audios, dependent: :destroy, foreign_key: :pmp_content_id
+  has_many :pmp_images, dependent: :destroy, foreign_key: :pmp_content_id
 
   def publish
     if content
@@ -36,7 +36,7 @@ class PmpStory < PmpContent
 
     if content.respond_to?(:audio)
       doc.links['item'].concat(content.audio.map do |a|
-        audio_content = a.parent || PmpAudio.create(content: a, pmp_story: self, profile: "audio")
+        audio_content = pmp_audios.first_or_create(content: a)
         audio_content.publish unless audio_content.published?
         audio_content.link
       end.compact)
@@ -44,7 +44,7 @@ class PmpStory < PmpContent
 
     if content.respond_to?(:assets)
       doc.links['item'].concat(content.assets.map do |i|
-        image_content = i.parent || PmpImage.create(content: i, pmp_story: self, profile: "image")
+        image_content = pmp_images.first_or_create(content: i)
         image_content.publish unless image_content.published?
         image_content.link
       end.compact)
