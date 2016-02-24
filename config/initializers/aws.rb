d855db8a9459a2138b5341adfb469327f38122e8
Ben Titcomb <btitcomb@scpr.org>
2016-02-24 10:05:34 -0800

    Change how docker machine variables are initialized and make AWS initializer not fail if the setting doesn't exist in secrets.yml.

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,6 @@
-Aws.config.update({
-  region: Rails.application.secrets.api['aws']['region'],
-  credentials: Aws::Credentials.new(Rails.application.secrets.api['aws']['access_key_id'], Rails.application.secrets.api['aws']['secret_access_key'])
-})
+if Rails.application.secrets.api['aws']
+  Aws.config.update({
+    region: Rails.application.secrets.api['aws']['region'],
+    credentials: Aws::Credentials.new(Rails.application.secrets.api['aws']['access_key_id'], Rails.application.secrets.api['aws']['secret_access_key'])
+  })
+end
