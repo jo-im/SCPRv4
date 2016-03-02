e2fe43e94995406a0b6c9130b47a2c7451e039ae
Ben Titcomb <btitcomb@scpr.org>
2016-03-01 17:11:45 -0800

    Add migration to republish pmp stories to reflect the template change.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229171922) do
+ActiveRecord::Schema.define(version: 20160302011026) do
 
   create_table "abstracts", force: :cascade do |t|
     t.string   "source",               limit: 255
