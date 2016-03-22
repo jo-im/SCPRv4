9f6258a298dbdc5ad2bbdbb5f491150ea60657b7
Ben Titcomb <btitcomb@scpr.org>
2016-03-22 13:37:17 -0700

    Missing comma added.

--- app/models/content_base/news_story.rb
+++ app/models/content_base/news_story.rb
@@ -105,7 +105,7 @@ class NewsStory < ActiveRecord::Base
       :published          => self.published?,
       :related_content    => related_content,
       :links              => related_links.map(&:to_hash),
-      :asset_display      => asset_display
+      :asset_display      => asset_display,
       :disqus_identifier  => self.disqus_identifier
     })
   end
