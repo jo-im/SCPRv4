f811509e9817d14f117f5c1f56919119695ba4a1
Ben Titcomb <btitcomb@scpr.org>
2016-06-17 11:14:24 -0700

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -91,17 +91,19 @@ ActiveRecord::Schema.define(version: 20160614184550) do
   add_index "bios_bio", ["slug"], name: "index_bios_bio_on_slug", using: :btree
 
   create_table "blogs_blog", force: :cascade do |t|
-    t.string   "name",           limit: 255
-    t.string   "slug",           limit: 255
-    t.text     "description",    limit: 4294967295
-    t.boolean  "is_active",                         default: false, null: false
-    t.string   "teaser",         limit: 255
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-    t.string   "twitter_handle", limit: 255
+    t.string   "name",                limit: 255
+    t.string   "slug",                limit: 255
+    t.text     "description",         limit: 4294967295
+    t.boolean  "is_active",                              default: false, null: false
+    t.string   "teaser",              limit: 255
+    t.integer  "missed_it_bucket_id", limit: 4
+    t.datetime "created_at",                                             null: false
+    t.datetime "updated_at",                                             null: false
+    t.string   "twitter_handle",      limit: 255
   end
 
   add_index "blogs_blog", ["is_active"], name: "index_blogs_blog_on_is_active", using: :btree
+  add_index "blogs_blog", ["missed_it_bucket_id"], name: "blogs_blog_d12628ce", using: :btree
   add_index "blogs_blog", ["name"], name: "index_blogs_blog_on_name", using: :btree
   add_index "blogs_blog", ["slug"], name: "slug", unique: true, using: :btree
 
@@ -148,7 +150,7 @@ ActiveRecord::Schema.define(version: 20160614184550) do
 
   create_table "caches", force: :cascade do |t|
     t.string "key",   limit: 255
-    t.text   "value", limit: 4294967295
+    t.binary "value", limit: 16777215
   end
 
   add_index "caches", ["key"], name: "index_caches_on_key", using: :btree
@@ -234,6 +236,53 @@ ActiveRecord::Schema.define(version: 20160614184550) do
   add_index "contentbase_contentshell", ["status", "published_at"], name: "index_contentbase_contentshell_on_status_and_published_at", using: :btree
   add_index "contentbase_contentshell", ["status"], name: "index_contentbase_contentshell_on_status", using: :btree
   add_index "contentbase_contentshell", ["updated_at"], name: "index_contentbase_contentshell_on_updated_at", using: :btree
+
+  create_table "contentbase_featuredcomment", force: :cascade do |t|
+    t.integer  "bucket_id",    limit: 4,          null: false
+    t.integer  "content_id",   limit: 4
+    t.integer  "status",       limit: 4,          null: false
+    t.string   "username",     limit: 255,        null: false
+    t.text     "excerpt",      limit: 4294967295, null: false
+    t.string   "content_type", limit: 255
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "contentbase_featuredcomment", ["bucket_id"], name: "contentbase_featuredcomment_25ef9024", using: :btree
+  add_index "contentbase_featuredcomment", ["content_type", "content_id"], name: "index_contentbase_featuredcomment_on_content_type_and_content_id", using: :btree
+  add_index "contentbase_featuredcomment", ["created_at"], name: "index_contentbase_featuredcomment_on_created_at", using: :btree
+  add_index "contentbase_featuredcomment", ["status"], name: "index_contentbase_featuredcomment_on_status", using: :btree
+
+  create_table "contentbase_featuredcommentbucket", force: :cascade do |t|
+    t.string   "title",      limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "contentbase_featuredcommentbucket", ["created_at"], name: "index_contentbase_featuredcommentbucket_on_created_at", using: :btree
+  add_index "contentbase_featuredcommentbucket", ["title"], name: "index_contentbase_featuredcommentbucket_on_title", using: :btree
+  add_index "contentbase_featuredcommentbucket", ["updated_at"], name: "index_contentbase_featuredcommentbucket_on_updated_at", using: :btree
+
+  create_table "contentbase_misseditbucket", force: :cascade do |t|
+    t.string   "title",      limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "slug",       limit: 255
+  end
+
+  add_index "contentbase_misseditbucket", ["title"], name: "index_contentbase_misseditbucket_on_title", using: :btree
+
+  create_table "contentbase_misseditcontent", force: :cascade do |t|
+    t.integer  "bucket_id",    limit: 4,                null: false
+    t.integer  "content_id",   limit: 4
+    t.integer  "position",     limit: 4,   default: 99, null: false
+    t.string   "content_type", limit: 255
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+  end
+
+  add_index "contentbase_misseditcontent", ["bucket_id"], name: "contentbase_misseditcontent_25ef9024", using: :btree
+  add_index "contentbase_misseditcontent", ["content_type", "content_id"], name: "index_contentbase_misseditcontent_on_content_type_and_content_id", using: :btree
 
   create_table "data_points", force: :cascade do |t|
     t.string   "group_name", limit: 255
@@ -444,13 +493,15 @@ ActiveRecord::Schema.define(version: 20160614184550) do
   add_index "layout_breakingnewsalert", ["visible"], name: "index_layout_breakingnewsalert_on_visible", using: :btree
 
   create_table "layout_homepage", force: :cascade do |t|
-    t.string   "base",         limit: 255
+    t.string   "base",                limit: 255
     t.datetime "published_at"
-    t.integer  "status",       limit: 4,   null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "status",              limit: 4,   null: false
+    t.integer  "missed_it_bucket_id", limit: 4
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
+  add_index "layout_homepage", ["missed_it_bucket_id"], name: "layout_homepage_d12628ce", using: :btree
   add_index "layout_homepage", ["published_at"], name: "index_layout_homepage_on_published_at", using: :btree
   add_index "layout_homepage", ["status", "published_at"], name: "index_layout_homepage_on_status_and_published_at", using: :btree
   add_index "layout_homepage", ["updated_at"], name: "index_layout_homepage_on_updated_at", using: :btree
