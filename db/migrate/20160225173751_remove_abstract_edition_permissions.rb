ccd774158577805acafa97a2f399c8584ee5f751
Ben Titcomb <btitcomb@scpr.org>
2016-06-17 09:51:07 -0700

    Revert "Revoke user permissions to use abstracts and editions in outpost."
    This reverts commit 374fcfa94e982a498f24d5b5f963de395b78a054.

--- db/migrate/20160225173751_remove_abstract_edition_permissions.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAbstractEditionPermissions < ActiveRecord::Migration
-  def up
-    if (ids = Permission.where(resource: ["Edition", "Abstract"]).pluck(:id)).any?
-      UserPermission.where(permission_id: ids).destroy_all
-    end
-  end
-  def down
-  end
-end
