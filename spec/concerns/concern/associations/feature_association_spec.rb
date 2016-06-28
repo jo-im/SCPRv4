aea704b23e9ba21e1504c68ccdf944f49bf9ef15
Ben Titcomb <btitcomb@scpr.org>
2016-06-28 09:44:50 -0700

    Auto-generate feature type on publish.

--- spec/concerns/concern/associations/feature_association_spec.rb
+++ spec/concerns/concern/associations/feature_association_spec.rb
@@ -13,6 +13,45 @@ describe Concern::Associations::FeatureAssociation do
 
       story.feature.should be_nil
     end
+
+    context "no feature id provided" do
+      context "on publish" do
+        context "has audio" do
+          it "assigns audio as feature type" do
+            story = create :test_class_story
+            story.audio << create(:audio, :external)
+            story.status = 5
+            story.save
+            expect(story.feature.key).to eq :audio
+          end
+        end
+        context "asset display is slideshow" do
+          it "assigns slideshow as feature type" do
+            story = create :test_class_story
+            story.asset_display = :slideshow
+            story.status = 5
+            story.save
+            expect(story.feature.key).to eq :slideshow
+          end
+        end
+        it "doesn't override existing feature type" do
+          story = create :test_class_story, feature_type_id: 3
+          story.asset_display = :slideshow
+          story.status = 5
+          story.save
+          expect(story.feature_type_id).to eq 3
+        end
+      end
+      context "before publish" do
+        it "does nothing" do
+          story = create :test_class_story
+          story.asset_display = :slideshow
+          story.status = 0
+          story.save
+          expect(story.feature).to be_nil
+        end
+      end
+    end
   end
 
 
