c8c97b02f109667b0a4b995face3e0aee8e190a3
Ben Titcomb <btitcomb@scpr.org>
2016-07-27 15:04:03 -0700

    Add broadcast content permission.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701162552) do
+ActiveRecord::Schema.define(version: 20160727220239) do
 
   create_table "abstracts", force: :cascade do |t|
     t.string   "source",               limit: 255
