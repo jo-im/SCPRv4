f79b5ab462b926c514ffc8ec03e316864860a575
Ben Titcomb <btitcomb@scpr.org>
2016-08-31 13:18:57 -0700

    Fix edge case with Embeditor processor that happens if we provide it with no input.

--- lib/embeditor/embeditor_processor.rb
+++ lib/embeditor/embeditor_processor.rb
@@ -17,7 +17,7 @@ module Embeditor
       Timeout.timeout(5) do
         @stdin.write html
         # We expect to receive at least an EOT byte from both stdout and stderr
-        output, error = [@stdout.gets("\x04"), @stderr.gets("\x04")].map{|o| o.gsub("\x04", "")}.map{|o| o.blank? ? nil : o}
+        output, error = [@stdout.gets("\x04"), @stderr.gets("\x04")].map{|o| (o || '').gsub("\x04", "")}.map{|o| o.blank? ? nil : o}
         unless error
           raise EmbeditorError.new("No output received.") if output.blank?
           return output.gsub("\x04", "") # remove our "end of transmission" signifier
