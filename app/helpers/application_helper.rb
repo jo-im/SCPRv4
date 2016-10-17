1275fa10857cbecb8ff46084b622863182e6a199
Ben Titcomb <btitcomb@scpr.org>
2016-10-17 14:55:56 -0700

    Re-enforce 48 hour rule on homepage related content.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -545,7 +545,7 @@ module ApplicationHelper
     related_content.select do |article|
       !@related_content_manifest.include?(article) &&
       !homepage_content_ids.include?(article.obj_key) && 
-      ((article.public_datetime || 10.years.ago)) &&
+      ((article.public_datetime || 10.years.ago) > 48.hours.ago) &&
       @related_content_manifest.push(article)
     end
     .first(2) ## We can assume here that we are already getting
