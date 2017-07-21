75bfc3b28ae710ad524fdc1a66623de3932cbcc0
Ben Titcomb <btitcomb@scpr.org>
2017-07-21 13:11:42 -0700

    Change how pledge settings are merged.

--- app/controllers/api/public/v3/settings_controller.rb
+++ app/controllers/api/public/v3/settings_controller.rb
@@ -2,8 +2,17 @@ module Api::Public::V3
   class SettingsController < BaseController
 
     def index
-      @settings = Setting.where(context: [params[:context], 'global']).where.not(context: nil)
+      @context  = [params[:context], 'global']
+      @settings = Setting.where(context: @context).where.not(context: nil).to_a
       @pledge_drive = PledgeDrive.happening.order("starts_at DESC").first
+      if @pledge_drive
+        @pledge_drive_setting = Setting.where(context: params[:context], key: "pledge_drive").first_or_initialize
+        @pledge_drive_setting.value ||= {}
+        @pledge_drive_setting.value["starts_at"] = @pledge_drive.starts_at
+        @pledge_drive_setting.value["ends_at"]   = @pledge_drive.ends_at
+        @settings.unshift @pledge_drive_setting
+        @settings.uniq!
+      end
       respond_with @settings
     end
     
--- app/views/api/public/v3/settings/index.json.jbuilder
+++ app/views/api/public/v3/settings/index.json.jbuilder
@@ -4,10 +4,4 @@ json.settings do
   @settings.each do |setting|
     json.set! setting.key, setting.value
   end
-  if @pledge_drive
-    json.pledge_drive do
-      json.starts_at @pledge_drive.starts_at
-      json.ends_at @pledge_drive.ends_at
-    end
-  end
 end
