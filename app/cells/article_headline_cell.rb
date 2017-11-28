f4110d31656df11f140b543a4c71630e5a169d58
arellaTV <jeremiah.arella@gmail.com>
2017-11-28 10:59:34 -0800

    Bumps the cache_key for articles and headlines

--- app/cells/article_headline_cell.rb
+++ app/cells/article_headline_cell.rb
@@ -2,7 +2,7 @@ class ArticleHeadlineCell < Cell::ViewModel
   property :category
 
   cache :show, expires_in: 10.minutes, :if => lambda { !@options[:preview] }  do
-    model.try(:cache_key)
+    [model.try(:cache_key), 'v2']
   end
 
   def show
