40708a11cbee65efdfb8e68d43e711df1cdea5d6
arellaTV <jeremiah.arella@gmail.com>
2017-12-08 16:39:07 -0800

    Fixes caching issue

--- app/cells/masthead_cell.rb
+++ app/cells/masthead_cell.rb
@@ -1,6 +1,6 @@
 class MastheadCell < Cell::ViewModel
   cache :show, expires_in: 12.hours do
-    ["masthead", "v2"]
+    ["masthead", "v2", model.try(:cache_key) || request.path]
   end
 
   def show
