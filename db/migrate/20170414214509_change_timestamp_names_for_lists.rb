a896e01a0a19ab74e8d9acb4d686b4c9d7f72f5e
arellaTV <jeremiah.arella@gmail.com>
2017-12-12 10:35:05 -0800

    Fixes alt text of program headers, adds 1A show logo, fixes styling

--- db/migrate/20170414214509_change_timestamp_names_for_lists.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTimestampNamesForLists < ActiveRecord::Migration
-  def change
-    rename_column :lists, :start_time, :starts_at
-    rename_column :lists, :end_time, :ends_at
-  end
-end
