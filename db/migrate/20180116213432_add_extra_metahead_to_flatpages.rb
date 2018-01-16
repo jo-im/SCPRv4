6b3058a8e3e455272065a597ade1f83cfe720a2c
arellaTV <jeremiah.arella@gmail.com>
2018-01-16 13:42:37 -0800

    Fix missing "end" typo in migration, and runs migration

--- db/migrate/20180116213432_add_extra_metahead_to_flatpages.rb
+++ db/migrate/20180116213432_add_extra_metahead_to_flatpages.rb
@@ -1,6 +1,7 @@
 class AddExtraMetaheadToFlatpages < ActiveRecord::Migration
   def up
     add_column :flatpages_flatpage, :extra_metahead, :text
+  end
   
   def down
     remove_column :flatpages_flatpage, :extra_metahead, :text
