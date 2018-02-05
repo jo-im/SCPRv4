063c7f5a14a5dec29faec9930d42a6704d5739bc
Ben Titcomb <btitcomb@scpr.org>
2018-02-05 11:41:05 -0800

    Fix sitemaps controller tests.

--- spec/controllers/sitemaps_controller_spec.rb
+++ spec/controllers/sitemaps_controller_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe SitemapsController do
   render_views
 
-  SitemapsController.sitemaps.each do |sitemap|
+  ["episodes", "segments", "queries", "blogs", "programs", "bios"].each do |sitemap|
     describe sitemap do
       it "sets @content" do
         get sitemap.to_sym
@@ -17,4 +17,20 @@ describe SitemapsController do
       end
     end
   end
+
+  ["stories", "blog_entries"].each do |sitemap|
+    describe sitemap do
+      it "sets @content" do
+        get sitemap.to_sym
+        assigns(:content).should_not be_nil
+      end
+
+      it "renders sitemap.xml" do
+        get sitemap.to_sym
+        response.should render_template 'news'
+        response.header['Content-Type'].should match /xml/
+      end
+    end
+  end
+
 end
