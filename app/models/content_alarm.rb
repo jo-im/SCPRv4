f19adf07f1c31eefdd6c177e7cb3c0ed573436eb
Louise Yang <louise.m.yang@gmail.com>
2018-03-09 14:48:39 -0800

    Bugfix if content for ContentAlarm doesn't actually exist

--- app/models/content_alarm.rb
+++ app/models/content_alarm.rb
@@ -28,20 +28,22 @@ class ContentAlarm < ActiveRecord::Base
   # able to call `fire` on any alarm at any time and have it perform its
   # action. If you want to batch-fire only pending alarms, use ::fire_pending.
   def fire
-    log "(##{self.id}) Firing Alarm (#{self.content.simple_title})"
+    if (self.content.present?)
+      log "(##{self.id}) Firing Alarm (#{self.content.simple_title})"
 
-    # No matter what, if the content is pending, then we want to
-    # destroy this alarm, otherwise it could get stuck forever
-    # or maybe publish unexpectedly in the future, which could be
-    # dangerous for something like BreakingNewsAlert.
-    if self.content.pending?
-      log "(##{self.id}) Content is pending... publishing it now. " \
+      # No matter what, if the content is pending, then we want to
+      # destroy this alarm, otherwise it could get stuck forever
+      # or maybe publish unexpectedly in the future, which could be
+      # dangerous for something like BreakingNewsAlert.
+      if self.content.pending?
+        log "(##{self.id}) Content is pending... publishing it now. " \
           "(#{self.content.simple_title})"
-      self.content.publish
-    else
-      log "(##{self.id} Content isn't pending. " \
+        self.content.publish
+      else
+        log "(##{self.id} Content isn't pending. " \
           "(#{self.content.simple_title})\n" \
           "Not publishing, but will still destroy this alarm."
+      end
     end
 
     # This method shouldn't be called unless you're ready to actually
