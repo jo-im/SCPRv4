7617aa548b2e5487aaea36e39615a71db0d3fe4e
arellaTV <jeremiah.arella@gmail.com>
2018-04-18 14:16:13 -0700

    Adds two more tests
    Updates article_id in remote_article factory
    
    Reverts factory with default article_id and temporrarily puts news_story
    
    Adds an inspect
    
    Temporarily put primary topic title to reference in circle ci
    
    Creates a custom category in the test
    
    Removes unnecessary puts and adds documentation

--- spec/importers/npr_article_importer_spec.rb
+++ spec/importers/npr_article_importer_spec.rb
@@ -39,6 +39,8 @@ describe NprArticleImporter do
     end
 
     it 'calls import after a successful sync' do
+      expect(NprArticleImporter).to receive(:import).at_least(:once)
+      NprArticleImporter.sync
     end
   end
 
@@ -86,6 +88,10 @@ describe NprArticleImporter do
     end
 
     it "adds a category if NPR's primaryTopic matches one of our categories" do
+      # Create a category with the title 'US & World'.
+      # This should match the response defined in the npr_article fixture (currently "World") - J.A.
+      create :category, title: 'US & World'
+
       remote_article = create :npr_article
       news_story = NprArticleImporter.import(remote_article)
       news_story.category.title.should eq 'US & World'
@@ -93,8 +99,17 @@ describe NprArticleImporter do
 
     it "does not call the API if options[:npr_story] is given" do
       remote_article = create :npr_article
-      npr_story = load_fixture('api/npr/story.json')
+
+      # Call the api once to get an NPR::Entity (id is taken from the fixture, 'api/npr/story.json')
+      npr_story = NPR::Story.find(187325945)
+
+      # Pass the NPR::Entity into the import method. Normally .import would perform an API call,
+      # but if an `npr_story` is already passed in, we want to use that instead
       news_story = NprArticleImporter.import(remote_article, { npr_story: npr_story })
+
+      # Webmock should only call once because we performed an NPR::Story.find earlier
+      # for the purposes of this test.
+      expect(WebMock).to have_requested(:get, %r|api\.npr|).once
     end
   end
 end
