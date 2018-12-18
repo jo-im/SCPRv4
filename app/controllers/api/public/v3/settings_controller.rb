18247509d549c6ad8e0754801459f6674a7daca5
Joann Im <joann_im@hotmail.com>
2018-12-18 02:22:44 -0800

    Add context for cache keys for settings

--- app/controllers/api/public/v3/settings_controller.rb
+++ app/controllers/api/public/v3/settings_controller.rb
@@ -4,7 +4,7 @@ module Api::Public::V3
     def index
       @context  = [params[:context], 'global']
 
-      @settings = Rails.cache.fetch('/api/v3/settings/#{params[:context]}', expires_in: 5.minutes) do
+      @settings = Rails.cache.fetch("/api/v3/settings/#{@context}", expires_in: 5.minutes) do
         settings = Setting.where(context: @context).where.not(context: nil).to_a
         pledge_drive = PledgeDrive.happening.order("starts_at DESC").first
 
