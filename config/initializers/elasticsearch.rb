b4128e2167b907da145172a495acb54a09d1238f
arellaTV <jeremiah.arella@gmail.com>
2019-05-30 10:41:41 -0700

    Initializes 30 second request timeout for ES ops
    Switches the timeout option to the faraday adapter
    
    Reduces timeout to 20 seconds

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,6 +2,9 @@ ES_CLIENT = Elasticsearch::Client.new(
   hosts:              Rails.configuration.x.scpr.elasticsearch_host || Rails.application.secrets.elasticsearch_host,
   retry_on_failure:   3,
   reload_connections: true,
+  transport_options: {
+    request: { timeout: 20 },
+  },
 )
 
 ES_PREFIX = Rails.configuration.x.scpr.elasticsearch_prefix || Rails.application.secrets.elasticsearch_prefix
